ecbb7c9165741b20f0975d1c9fe8ed0149f5c8ea
brianartea <bri@expertdesign.cc>
2021-09-03 17:43:12 -0400

    commit

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Showcase.module.css";
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the developers community</h1>
+      <h2>Great web and app development resources</h2>
+    </div>
+  );
+}
